bf32ce77584ad457800bd783298bfd96b4f3d3e0
Anhphuc9969 <you@example.com>
2021-11-22 21:30:57 +0700

    up security

--- src/main/java/com/example/recruitmentwebsitesystem/repo/UsersRepo.java
+++ src/main/java/com/example/recruitmentwebsitesystem/repo/UsersRepo.java
@@ -1,8 +1,17 @@
 package com.example.recruitmentwebsitesystem.repo;
 
-import com.example.recruitmentwebsitesystem.entity.User;
+import com.example.recruitmentwebsitesystem.entity.Users;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsersRepo extends BaseRepo<User, Integer> {
+public interface UsersRepo extends BaseRepo<Users, Integer> {
+    Users findByUsername(String user_name);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+
+    Users findByOtpCode(String code);
+
+    Users findByEmail(String email);
 }
--- src/main/java/com/example/recruitmentwebsitesystem/service/UsersService.java
+++ src/main/java/com/example/recruitmentwebsitesystem/service/UsersService.java
@@ -1,8 +1,13 @@
 package com.example.recruitmentwebsitesystem.service;
 
-import com.example.recruitmentwebsitesystem.entity.User;
+import com.example.recruitmentwebsitesystem.entity.Users;
 import com.example.recruitmentwebsitesystem.generic.BaseService;
 
-public interface UsersService extends BaseService<User, Integer> {
+public interface UsersService extends BaseService<Users, Integer> {
+    Users findUserByEmail(String email);
+    Users findByName(String name);
+    Users save(Users user);
+    boolean existByUserName(String username);
+    boolean existByEmail(String email);
 }
 
